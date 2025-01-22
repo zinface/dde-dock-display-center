7084072bf2141dab61ab5af1b62b03cf2f3717ee
zinface <zinface@163.com>
2025-01-22 09:35:50 +0800

    fix(dde-dock): 修复重启后时尚模式和效率模式不能正常切换的问题
    - 在脚本中添加睡眠时间，确保模式切换生效
    - 获取显示模式的范围，并根据范围设置经典模式
    - 优化脚本执行流程，提高模式切换的可靠性

--- dde-dock-reset-display.sh
+++ dde-dock-reset-display.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 /usr/bin/killall dde-dock
 /usr/bin/dde-dock
+
 sleep 1
 gsettings set com.deepin.dde.dock display-mode 'fashion'
+sleep 0.5
 gsettings set com.deepin.dde.dock display-mode 'efficient'
-gsettings set com.deepin.dde.dock display-mode 'classic'
+sleep 0.5
+
+IFS=$'\n'
+RANGE=$(gsettings range com.deepin.dde.dock display-mode)
+
+# enum 'fashion' 'efficient' 'classic'
+if [[ "${RANGE}" == *"classic"* ]]; then
+    echo "contains: classic"
+    gsettings set com.deepin.dde.dock display-mode 'classic'
+fi
